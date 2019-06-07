87ab21a251d1c3096a418bab3b11c2658fe061b1
chintan <chintan.adatiya@gmail.com>
2019-06-07 17:40:31 +0530

    Hook working with mysql

--- index.js
+++ index.js
@@ -4,45 +4,47 @@
  * @description :: A hook definition.  Extends Sails by adding shadow routes, implicit actions, and/or initialization logic.
  * @docs        :: https://sailsjs.com/docs/concepts/extending-sails/hooks
  */
-module.exports = function jobqueue(sails) {
+module.exports = function softdelete(sails) {
 
   return {
     initialize: async function () {
-      // sails
-      // .after(['hook:userhooks:loaded'], function() {
-      //   // init();
-      //   console.error('gyu ho ', sails.models);
-      // });
       //   var hook = this;
       var eventsToWaitFor = [];
-      if (sails.hooks.orm) {
-        eventsToWaitFor.push('hook:orm:loaded');
+      if (sails.hooks.userhooks) {
+        eventsToWaitFor.push('hook:userhooks:loaded');
       }
 
-      if (sails.hooks.pubsub) {
-        eventsToWaitFor.push('hook:pubsub:loaded');
-      }
+      // // if (sails.hooks.orm) {
+      // //   eventsToWaitFor.push('hook:orm:loaded');
+      // // }
+
+      // // if (sails.hooks.pubsub) {
+      // //   eventsToWaitFor.push('hook:pubsub:loaded');
+      // // }
 
       sails.after(eventsToWaitFor, function () {
-        sails.log.info(" 🍺   Logistic Infotech's sails-hook-softdelete loaded 🍺  ");
         init();
+        sails.log.info(" 🍺   Logistic Infotech's sails-hook-softdelete loaded 🍺  ");
       });
     }
   };
 
   function init() {
-    _(sails.models)
-      .forEach(function (model) {
-        if (model.softDelete) {
-          console.log('gyu ho ', model.attributes);
-          _.extend(model.attributes, {
-            deletedAt1: {
-              type: 'datetime',
-              defaultsTo: null
-            }
-          });
-        }
-      });
+    // console.error('init ', sails.models);
+    _.each(sails.models, function (model, modelkey) {
+      // console.error('init forEach ', model);
+      if (model.softDelete) {
+        console.log('model.softDelete true found ', modelkey);
+        _.extend(model.attributes, {
+          deletedAt: {
+            type: 'ref',
+            columnType: 'datetime',
+            defaultsTo: null
+          }
+        });
+      }
+    });
+
   }
 
 };
